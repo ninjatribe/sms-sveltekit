838e0fd105126a895732bea47f61d1ca3b2fb532
TheTrackLP <113531678+TheTrackLP@users.noreply.github.com>
2023-03-07 13:39:38 +0800

    Navbar Added to the forms folder

---
+++ src/lib/components/forms/Navbar.svelte
@@ -0,0 +1,24 @@
+<nav class="p-5 border-gray-200 bg-gray-50 dark:bg-gray-800 dark:border-gray-700">
+    <div class="container flex flex-wrap items-center justify-between mx-auto">
+      <a href="/" class="flex items-center">
+        <img src="https://flowbite.com/docs/images/logo.svg" class="h-6 mr-3 sm:h-9" alt="Flowbite Logo" />
+        <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">FARS</span>
+      </a>
+      <div class="w-full md:block md:w-auto" id="navbar-solid-bg">
+        <ul class="flex flex-col mt-4 rounded-lg bg-gray-50 md:flex-row md:space-x-14 md:mt-0 md:text-sm md:font-bold md:border-0 md:bg-transparent dark:bg-gray-800 md:dark:bg-transparent dark:border-gray-700">
+          <li>
+            <a href="/" class="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-white-700 md:p-2 dark:text-gray-400 md:dark:hover:text-dark-blue dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Home</a>
+          </li>
+          <li>
+            <a href="/" class="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-white-700 md:p-2 dark:text-gray-400 md:dark:hover:text-dark-blue dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">About</a>
+          </li>
+          <li>
+            <a href="/" class="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-white-700 md:p-2 dark:text-gray-400 md:dark:hover:text-dark-blue dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Serve</a>
+          </li>
+          <li>
+            <a href="/" class="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-white-700 md:p-2 dark:text-gray-400 md:dark:hover:text-dark-blue dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Contact</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
