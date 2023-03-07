e90f33c2650f8c595b558d9efaca229f86e01511
iaiaian1 <yapjakebrian1@gmail.com>
2023-03-07 11:10:04 +0800

    Component dashboard added. Subject to heavy design. I dont know what the final design would look like but this should do!

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -87,6 +87,10 @@
 			class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mx-2"
 			on:click={DownloadHandler}>Download</button
 		>
+		<button
+			class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mx-2"
+			><a href="/dashboard">Dashboard</a></button
+		>
 		<input
 			type="search"
 			bind:value={search}
