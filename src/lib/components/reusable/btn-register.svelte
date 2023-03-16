125a09fc92e50aebfc27eea6dc9a8ac93d547c31
Villy Joe Delsocora <NinjaOJT_Delsocora@outlook.com>
2023-03-16 12:14:33 +0800

    Redesign Log in page using components

---
+++ src/lib/components/reusable/btn-register.svelte
@@ -0,0 +1,7 @@
+<button
+type="button"
+class="inline-block rounded border-2 border-danger px-6 pt-2 pb-[6px] text-xs font-medium uppercase leading-normal text-danger transition duration-150 ease-in-out hover:border-danger-600 hover:bg-neutral-500 hover:bg-opacity-10 hover:text-danger-600 focus:border-danger-600 focus:text-danger-600 focus:outline-none focus:ring-0 active:border-danger-700 active:text-danger-700 dark:hover:bg-neutral-100 dark:hover:bg-opacity-10"
+data-te-ripple-init
+data-te-ripple-color="light">
+Register
+</button>
--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,3 +1,11 @@
+<script>
+	// @ts-nocheck
+	import InputCred from '$lib/components/reusable/InputCred.svelte'
+	import BtnLogin from '$lib/components/reusable/btn-Login.svelte'
+	import BtnRegister from '$lib/components/reusable/btn-register.svelte'
+
+</script>
+
 <section class="gradient-form h-full bg-neutral-200 dark:bg-neutral-700 h-screen">
 	<div class="container h-full p-10">
 	  <div
@@ -19,52 +27,15 @@
 				  </div>
 				  <form>
 					<p class="mb-4">Please login to your account</p>
-					<div class="relative mb-4" data-te-input-wrapper-init>
-					  <input
-						type="text"
-						class="peer block min-h-[auto] w-full rounded border-0 bg-transparent py-[0.32rem] px-3 leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:placeholder:text-neutral-200 [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
-						id="exampleFormControlInput1"
-						placeholder="Username" />
-					  <label
-						for="exampleFormControlInput1"
-						class="pointer-events-none absolute top-0 left-3 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] peer-focus:text-blue-600 peer-data-[te-input-state-active]:-translate-y-[0.9rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-200 dark:peer-focus:text-neutral-200"
-						>Username
-					  </label>
-					</div>
-					<div class="relative mb-4" data-te-input-wrapper-init>
-					  <input
-						type="password"
-						class="peer block min-h-[auto] w-full rounded border-0 bg-transparent py-[0.32rem] px-3 leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:placeholder:text-neutral-200 [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
-						id="exampleFormControlInput11"
-						placeholder="Password" />
-					  <label
-						for="exampleFormControlInput11"
-						class="pointer-events-none absolute top-0 left-3 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] peer-focus:text-blue-600 peer-data-[te-input-state-active]:-translate-y-[0.9rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-200 dark:peer-focus:text-neutral-200"
-						>Password
-					  </label>
-					</div>
+					<InputCred label = "Username"/>
+					<InputCred label = "Password"/>
 					<div class="mb-12 pt-1 pb-1 text-center">
-					  <button
-						class="mb-3 inline-block w-full rounded px-6 pt-2.5 pb-2 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_rgba(0,0,0,0.2)] transition duration-150 ease-in-out hover:shadow-[0_8px_9px_-4px_rgba(0,0,0,0.1),0_4px_18px_0_rgba(0,0,0,0.2)] focus:shadow-[0_8px_9px_-4px_rgba(0,0,0,0.1),0_4px_18px_0_rgba(0,0,0,0.2)] focus:outline-none focus:ring-0 active:shadow-[0_8px_9px_-4px_rgba(0,0,0,0.1),0_4px_18px_0_rgba(0,0,0,0.2)]"
-						type="button"
-						data-te-ripple-init
-						data-te-ripple-color="light"
-						style="
-						  background: linear-gradient(to right, #ee7724, #d8363a, #dd3675, #b44593);
-						">
-						Log in
-					  </button>
+					  <BtnLogin/>
 					  <a href="#!">Forgot password?</a>
 					</div>
 					<div class="flex items-center justify-between pb-6">
 					  <p class="mb-0 mr-2">Don't have an account?</p>
-					  <button
-						type="button"
-						class="inline-block rounded border-2 border-danger px-6 pt-2 pb-[6px] text-xs font-medium uppercase leading-normal text-danger transition duration-150 ease-in-out hover:border-danger-600 hover:bg-neutral-500 hover:bg-opacity-10 hover:text-danger-600 focus:border-danger-600 focus:text-danger-600 focus:outline-none focus:ring-0 active:border-danger-700 active:text-danger-700 dark:hover:bg-neutral-100 dark:hover:bg-opacity-10"
-						data-te-ripple-init
-						data-te-ripple-color="light">
-						Register
-					  </button>
+					  <BtnRegister/>
 					</div>
 				  </form>
 				</div>
