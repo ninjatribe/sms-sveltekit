162f175d86f9df31f300bc2da02fdb78267d3605
Mark <markjungersaniva@gmail.com>
2023-03-18 16:09:20 +0800

    feat: updated login

--- src/routes/auth/login/+page.svelte
+++ src/routes/auth/login/+page.svelte
@@ -3,9 +3,13 @@
 	import { page } from '$app/stores';
 	import { goto } from '$app/navigation';
 	import { SHA256 } from 'crypto-js';
-	
+	import InputCred from '$lib/components/reusable/InputCred.svelte';
+	import ButtonGradient from '$lib/components/reusable/ButtonGradient.svelte';
+	import Button from '$lib/components/reusable/Button.svelte';
+	import Alert from '$lib/components/reusable/Alert.svelte';
+
 	export let data;
-	const {user} = data;
+	const { user } = data;
 
 	let email = '';
 	let password = '';
@@ -19,7 +23,7 @@
 			setTimeout(() => {
 				hasAccess = false;
 			}, 500);
-			goto('/auth/login')
+			goto('/auth/login');
 		}
 	}
 
@@ -28,13 +32,13 @@
 		const response = await fetch('/api/auth/login', {
 			method: 'POST',
 			headers: {
-				'content-type': 'application/json',
+				'content-type': 'application/json'
 			},
-			body: JSON.stringify({ email, password: securePassword}),
+			body: JSON.stringify({ email, password: securePassword })
 		});
 		const data = await response.json();
-
-		if(data.error) {
+		console.log(data);
+		if (data.error) {
 			error = data.errorMessage || 'An error occured';
 			loggingIn = false;
 		} else {
@@ -49,106 +53,76 @@
 						photo: data.user.profile.photo,
 						country: data.user.profile.country,
 						province: data.user.profile.province,
-						displayName: data.user.profile.displayName,
+						displayName: data.user.profile.displayName
 					},
-					email: data.user.profile.email,
+					email: data.user.profile.email
 				};
 			});
-			goto('/dashboard')
+			goto('/dashboard');
 		}
-	}
-
+	};
 </script>
 
-<section class="gradient-form bg-neutral-200 dark:bg-neutral-700 h-screen">
+<section class="gradient-form h-full bg-neutral-200 dark:bg-neutral-700">
 	<div class="container h-full p-10">
-	  <div
-		class="g-6 flex h-full flex-wrap items-center justify-center text-neutral-800 dark:text-neutral-200">
-		<div class="w-full">
-		  <div
-			class="block rounded-lg bg-white shadow-lg dark:bg-neutral-800">
-			<div class="g-0 lg:flex lg:flex-wrap">
-			  <div class="px-4 md:px-0 lg:w-6/12">
-				<div class="md:mx-6 md:p-12">
-				  <div class="text-center">
-					<h4 class="mt-1 mb-1 pb-1 text-xl font-semibold">
-						WELCOME TO
-					  </h4>
-					<img
-					  class="mx-auto w-48 mb-12"
-					  src="https://faith.edu.ph/wp-content/uploads/2020/11/faith-academy-logo.svg"
-					  alt="logo" />
-				  </div>
-				  <form method="POST" autocomplete="off"
-				  on:submit={(e) => {
-					  e.preventDefault();
-					  if(!loggingIn) {
-						  loggingIn = true;
-						  handleLogin();
-					  }
-				  }}>
-					<p class="mb-4">Please login to your account</p>
-					<div class="relative mb-4" data-te-input-wrapper-init>
-					  <input
-						type="email"
-						class="peer block min-h-[auto] w-full rounded border-0 bg-transparent py-[0.32rem] px-3 leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:placeholder:text-neutral-200 [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
-						id="email"
-						bind:value={email}
-						placeholder="Email" />
-					  <label
-						for="email"
-						class="pointer-events-none absolute top-0 left-3 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] peer-focus:text-blue-600 peer-data-[te-input-state-active]:-translate-y-[0.9rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-200 dark:peer-focus:text-neutral-200"
-						>Email
-					  </label>
+		<div
+			class="g-6 flex h-full flex-wrap items-center justify-center text-neutral-800 dark:text-neutral-200"
+		>
+			<div class="w-full">
+				<div class="block rounded-lg bg-white shadow-lg dark:bg-neutral-800">
+					<div class="g-0 lg:flex lg:flex-wrap">
+						<div class="px-4 md:px-0 lg:w-6/12">
+							<div class="md:mx-6 md:p-12">
+								<div class="text-center">
+									<h4 class="mt-1 mb-1 pb-1 text-xl font-semibold">WELCOME TO</h4>
+									<img
+										class="mx-auto w-48 mb-12"
+										src="https://faith.edu.ph/wp-content/uploads/2020/11/faith-academy-logo.svg"
+										alt="logo"
+									/>
+								</div>
+								<form
+									method="POST"
+									autocomplete="off"
+									on:submit={(e) => {
+										e.preventDefault();
+										if (!loggingIn) {
+											loggingIn = true;
+											handleLogin();
+										}
+									}}
+								>
+									<p class="mb-4">Please login to your account</p>
+									<InputCred label="Email" name="email" type="email" bind:value={email} />
+									<InputCred label="Password" name="password" type="password" bind:value={password} />
+									<div class="mb-12 pt-1 pb-1 text-center">
+										<ButtonGradient type={'submit'}>Login</ButtonGradient>
+										<a href="#!">Forgot password?</a>
+									</div>
+									<div class="flex items-center justify-between pb-6">
+										<p class="mb-0 mr-2">Don't have an account?</p>
+										<Button>Register</Button>
+									</div>
+								</form>
+								{#if error}
+									<Alert message={error} bgColor={'bg-red-500'} />
+								{/if}
+							</div>
+						</div>
+						<div
+							class="flex items-center rounded-b-lg lg:w-6/12 lg:rounded-r-lg lg:rounded-bl-none"
+						>
+							<div class="text-white w-full h-full relative text-justify align-middle">
+								<img
+									class="w-full h-full object-cover rounded-r-lg"
+									src="https://faith.edu.ph/wp-content/uploads/2020/12/IMG_8597-19-1-1024x682.jpg"
+									alt=""
+								/>
+							</div>
+						</div>
 					</div>
-					<div class="relative mb-4" data-te-input-wrapper-init>
-					  <input
-						type="password"
-						class="peer block min-h-[auto] w-full rounded border-0 bg-transparent py-[0.32rem] px-3 leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:placeholder:text-neutral-200 [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
-						id="password"
-						bind:value={password}
-						placeholder="Password" />
-					  <label
-						for="password"
-						class="pointer-events-none absolute top-0 left-3 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] peer-focus:text-blue-600 peer-data-[te-input-state-active]:-translate-y-[0.9rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-200 dark:peer-focus:text-neutral-200"
-						>Password
-					  </label>
-					</div>
-					<div class="mb-12 pt-1 pb-1 text-center">
-					  <button
-						class="mb-3 inline-block w-full rounded px-6 pt-2.5 pb-2 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_rgba(0,0,0,0.2)] transition duration-150 ease-in-out hover:shadow-[0_8px_9px_-4px_rgba(0,0,0,0.1),0_4px_18px_0_rgba(0,0,0,0.2)] focus:shadow-[0_8px_9px_-4px_rgba(0,0,0,0.1),0_4px_18px_0_rgba(0,0,0,0.2)] focus:outline-none focus:ring-0 active:shadow-[0_8px_9px_-4px_rgba(0,0,0,0.1),0_4px_18px_0_rgba(0,0,0,0.2)]"
-						type="submit"
-						data-te-ripple-init
-						data-te-ripple-color="light"
-						style="
-						  background: linear-gradient(to right, #ee7724, #d8363a, #dd3675, #b44593);
-						">
-						Log in
-					  </button>
-					  <a href="#!">Forgot password?</a>
-					</div>
-					<div class="flex items-center justify-between pb-6">
-					  <p class="mb-0 mr-2">Don't have an account?</p>
-					  <button
-						type="button"
-						class="inline-block rounded border-2 border-danger px-6 pt-2 pb-[6px] text-xs font-medium uppercase leading-normal text-danger transition duration-150 ease-in-out hover:border-danger-600 hover:bg-neutral-500 hover:bg-opacity-10 hover:text-danger-600 focus:border-danger-600 focus:text-danger-600 focus:outline-none focus:ring-0 active:border-danger-700 active:text-danger-700 dark:hover:bg-neutral-100 dark:hover:bg-opacity-10"
-						data-te-ripple-init
-						data-te-ripple-color="light">
-						Register
-					  </button>
-					</div>
-				  </form>
 				</div>
-			  </div>
-			  <div
-				class="flex items-center rounded-b-lg lg:w-6/12 lg:rounded-r-lg lg:rounded-bl-none">
-				<div class="text-white w-full h-full relative text-justify align-middle">
-					<img class="w-full h-full object-cover rounded-r-lg" src="https://faith.edu.ph/wp-content/uploads/2020/12/IMG_8597-19-1-1024x682.jpg" alt="">
-				</div>
-			  </div>
 			</div>
-		  </div>
 		</div>
-	  </div>
 	</div>
-  </section>
+</section>
