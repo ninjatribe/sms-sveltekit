38f678070a734462e6375dfdda74c78756929c6c
Mark <markjungersaniva@gmail.com>
2023-03-18 16:09:54 +0800

    feat: added components

--- src/lib/components/reusable/ButtonGradient.svelte
+++ src/lib/components/reusable/ButtonGradient.svelte
@@ -1,9 +1,13 @@
+<script>
+    // @ts-nocheck
+    export let type = 'button';
+</script>
 <button
     class="mb-3 inline-block w-full rounded px-6 pt-2.5 pb-2 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_rgba(0,0,0,0.2)] transition duration-150 ease-in-out hover:shadow-[0_8px_9px_-4px_rgba(0,0,0,0.1),0_4px_18px_0_rgba(0,0,0,0.2)] focus:shadow-[0_8px_9px_-4px_rgba(0,0,0,0.1),0_4px_18px_0_rgba(0,0,0,0.2)] focus:outline-none focus:ring-0 active:shadow-[0_8px_9px_-4px_rgba(0,0,0,0.1),0_4px_18px_0_rgba(0,0,0,0.2)]"
-    type="button"
+    type="{type}"
     data-te-ripple-init
     data-te-ripple-color="light"
     style="
     background: linear-gradient(to right bottom, rgb(134, 239, 172), rgb(59, 130, 246), rgb(0, 0, 139));
-    ">LOG IN
+    "><slot/>
 </button>
---
+++ src/lib/components/reusable/HyperLink.svelte
@@ -0,0 +1,15 @@
+<script>
+    // @ts-nocheck
+    export let buttonName, href,img;
+</script>
+
+<div class="h-10 bg-white dark:bg-gray-800">
+    <ul class="space-y-2">
+       <li>
+          <a href={href} class="flex items-center p-2 text-base font-semibold text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700" style="text-decoration-line: none;">
+             <svg aria-hidden="true" class="w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d={img}></svg>
+             <span class="ml-3">{buttonName}</span>
+          </a>
+       </li>
+    </ul>
+</div>
