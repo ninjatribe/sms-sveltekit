0b357548638aa9035531ee9af2a826ede26acae2
Mark <markjungersaniva@gmail.com>
2023-07-17 16:59:33 +0800

    update: navbar

---
+++ src/lib/components/home/Navbar.svelte
@@ -0,0 +1,27 @@
+<script>
+    import faith_logo from '$lib/images/faith_academy_letter_logo.svg'
+	import Hamburger from '$lib/components/reusable/Hamburger.svelte';
+
+    // Hamburger/sidebar
+    export let open = false;
+</script>
+<!-- Blank space -->
+<div class="h-10 bg-blue-900">
+    
+</div>
+<!-- Custom navbar wrapper -->
+<div class="flex items-centers justify-center p-2 w-full">
+    <a href="/" class="w-full flex justify-start lg:justify-center">
+        <img src={faith_logo} alt="faith_logo" class="h-20">
+    </a>
+    <!-- Buttons -->
+    <div class="hidden lg:flex justify-center items-center gap-8 font-bold text-blue-900 uppercase w-full">
+        <a href="#" class="hover:text-slate-400 transition-all duration-200 ease-in">About</a>
+        <a href="/apply" class="hover:text-slate-400 transition-all duration-200 ease-in">Apply</a>
+        <a href="#" class="hover:text-slate-400 transition-all duration-200 ease-in">Serve</a>
+        <a href="#" class="hover:text-slate-400 transition-all duration-200 ease-in">School Life</a>
+    </div>
+    <div class="flex lg:hidden justify-center items-center w-1/4">
+        <Hamburger bind:open/>
+    </div>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,17 +1,15 @@
 <script>
     import { onMount } from "svelte/internal";
 
-    import faith_logo from '$lib/images/faith_academy_letter_logo.svg'
     import faith_logo_white from '$lib/images/faith_academy_letter_logo_white.svg'
-    import arrow_right from '$lib/images/arrow_right.svg'
     import main_home from '$lib/images/main_home_img.jpeg'
     import group_photo from '$lib/images/group_photo.jpeg'
     import elementary from '$lib/images/elementary.jpg'
     import middle_school from '$lib/images/middle_school.jpg'
     import high_school from '$lib/images/high_school.jpg'
+	import Navbar from "$lib/components/home/Navbar.svelte";
     
     import Footer from '$lib/components/Footer.svelte'
-	import Hamburger from '$lib/components/reusable/Hamburger.svelte';
 
     //Socials
     import facebook_logo from '$lib/images/facebook_logo.svg'
@@ -19,7 +17,6 @@
     import vimeo_logo from '$lib/images/vimeo_logo.svg'
     import youtube_logo from '$lib/images/youtube_logo.svg'
 
-    // Hamburger/sidebar
     export let open = false;
 
     // Adjust embedded vimeo width/height
@@ -86,26 +83,7 @@
 </script>
 
 <div class="main-wrapper">
-    <!-- Blank space -->
-    <div class="h-10 bg-blue-900">
-    
-    </div>
-    <!-- Custom navbar wrapper -->
-    <div class="flex items-centers justify-center p-2 w-full">
-        <a href="/" class="w-full flex justify-start lg:justify-center">
-            <img src={faith_logo} alt="faith_logo" class="h-20">
-        </a>
-        <!-- Buttons -->
-        <div class="hidden lg:flex justify-center items-center gap-8 font-bold text-blue-900 uppercase w-full">
-            <a href="/about" class="hover:text-slate-400 transition-all duration-200 ease-in">About</a>
-            <a href="/admissions" class="hover:text-slate-400 transition-all duration-200 ease-in">Admissions</a>
-            <a href="/serve" class="hover:text-slate-400 transition-all duration-200 ease-in">Serve</a>
-            <a href="/SchoolLife" class="hover:text-slate-400 transition-all duration-200 ease-in">School Life</a>
-        </div>
-        <div class="flex lg:hidden justify-center items-center w-1/4">
-            <Hamburger bind:open/>
-        </div>
-    </div>
+    <Navbar />
 
     <!-- 1st image -->
     <div class="mt-2 w-full h-full bg-center bg-cover bg-fixed" style="background-image: url({main_home})" >
